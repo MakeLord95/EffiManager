b06a64b176bd119e7109daebf5e405b32ba3beff
MakeLord95 <juhani.markus@gmail.com>
2023-12-19 03:25:10 +0200

    Backend start

---
+++ backend/config/db.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+require("dotenv").config();
+
+const MONGO_URI = process.env.MONGO_URI;
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(MONGO_URI);
+    console.log("MongoDB connection SUCCESS");
+  } catch (error) {
+    console.error("MongoDB connection FAIL");
+    process.exit(1);
+  }
+};
+
+export default connectDB;
---
+++ backend/models/userModel.js
@@ -0,0 +1,132 @@
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+import validator from "validator";
+
+// User schema
+const userSchema = mongoose.Schema(
+  {
+    name: {
+      type: "string",
+      required: true,
+      maxlength: 32,
+    },
+    email: {
+      type: "string",
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: "string",
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+// Sign up user
+userSchema.statics.signup = async function (name, email, password) {
+  if (!validator.isAlpha(name)) {
+    throw new Error("Name can only contain letters");
+  }
+
+  if (!validator.isEmail(email)) {
+    throw new Error("Invalid email");
+  }
+
+  if (!validator.isStrongPassword(password)) {
+    throw new Error(
+      "Password must be at least 8 characters long and contain at least one uppercase letter, one lowercase letter, one number and one symbol"
+    );
+  }
+
+  if (this.findOne({ email })) {
+    throw new Error("User already exists");
+  }
+
+  try {
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = await this.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    return user;
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+// Login user
+userSchema.statics.login = async function (email, password) {
+  if (!validator.isEmail(email)) {
+    throw new Error("Invalid email");
+  }
+
+  try {
+    const user = await this.findOne({ email });
+
+    if (!user) {
+      throw new Error("User does not exist");
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      throw new Error("Invalid password");
+    }
+
+    return user;
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+// Update user
+userSchema.statics.updateUser = async function (id, name, email, password) {
+  if (!validator.isAlpha(name)) {
+    throw new Error("Name can only contain letters");
+  }
+
+  if (!validator.isEmail(email)) {
+    throw new Error("Invalid email");
+  }
+
+  if (!validator.isStrongPassword(password)) {
+    throw new Error(
+      "Password must be at least 8 characters long and contain at least one uppercase letter, one lowercase letter, one number and one symbol"
+    );
+  }
+
+  try {
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = await this.findByIdAndUpdate(
+      id,
+      {
+        name,
+        email,
+        password: hashedPassword,
+      },
+      { new: true }
+    );
+
+    return user;
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+// Delete user
+userSchema.statics.deleteUser = async function (id) {
+  try {
+    const user = await this.findByIdAndDelete(id);
+
+    return user;
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+module.exports = mongoose.model("User", userSchema);
