ca4e96e4125d8285cfc3e2c84b703109b7754262
MakeLord95 <juhani.markus@gmail.com>
2024-01-24 16:56:58 +0200

    Home page fix

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,6 +2,7 @@
 import { Link } from "react-router-dom";
 
 export default function Home({ isAuthenticated }) {
+  console.log(isAuthenticated);
   return (
     <section className="hero" id="home">
       {!isAuthenticated && (
@@ -9,7 +10,19 @@ export default function Home({ isAuthenticated }) {
           <h1>EffiManager</h1>
           <p>Efficiently manage your personal tasks</p>
           <Link to="/register" className="btn btn-hero">
-            Get Started
+            Register
+          </Link>
+          <Link to="/login" className="btn btn-hero">
+            Login
+          </Link>
+        </div>
+      )}
+      {isAuthenticated && (
+        <div className="hero-banner">
+          <h1>EffiManager</h1>
+          <p>Efficiently manage your personal tasks</p>
+          <Link to="/dashboard" className="btn btn-hero">
+            Dashboard
           </Link>
         </div>
       )}
