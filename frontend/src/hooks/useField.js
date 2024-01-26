f6a72dd549fd0c397b6a14309575e664d41ffd87
make <juhani.markus@gmail.com>
2024-01-26 11:12:13 +0200

    Start work on register

---
+++ frontend/src/hooks/useField.js
@@ -0,0 +1,18 @@
+// useField.js
+import { useState } from "react";
+
+const useField = (type) => {
+  const [value, setValue] = useState("");
+
+  const onChange = (event) => {
+    setValue(event.target.value);
+  };
+
+  return {
+    type,
+    value,
+    onChange,
+  };
+};
+
+export default useField;
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,8 +1,39 @@
+import useField from "../hooks/useField";
+
 export default function Register() {
+  const email = useField("email");
+  const password = useField("password");
+
+  const handleRegister = (e) => {
+    e.preventDefault();
+    console.log(email.value, password.value);
+  };
+
   return (
     <div>
-      <h1>Register</h1>
-      <p>This is the register page.</p>
+      <form className="register-form">
+        <h2 className="register-title">Register</h2>
+
+        <label className="email-label" htmlFor="email">
+          Email
+        </label>
+
+        <input {...email} className="email-input" />
+
+        <br />
+
+        <label htmlFor="password" className="password-label">
+          Password
+        </label>
+
+        <input {...password} className="password-input" />
+
+        <br />
+
+        <button onClick={handleRegister} className="register-button">
+          Register
+        </button>
+      </form>
     </div>
   );
 }
